7e44592cbc1ef629019bcfacc54d6cabbfb0bd24
VanhoorneSam <Sam.vanhoorne@student.howest.be>
2018-02-19 11:02:33 +0100

    Add project files.

---
+++ DotNetCoreMVCVb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotNetCoreMVCVb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
